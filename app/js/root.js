f35172dc081a234740b2faf72e020171961f7036
王晓峰 <qingduan@live.com>
2017-05-18 08:13:08 +0800

    eslint

--- app/js/root.js
+++ app/js/root.js
@@ -1,7 +1,8 @@
 /**
  * Created by wangxf on 2017/5/8.
  */
-import React, {Component} from 'react';
+
+import React, { Component } from 'react';
 import {
     View,
     Image,
@@ -9,136 +10,134 @@ import {
     Platform,
     BackHandler,
     ToastAndroid,
-    StyleSheet
+    StyleSheet,
 } from 'react-native';
 import Login from './main/login/Login';
 import Splash from './main/login/Splash';
 import AppStorage from './utils/storage';
 import AppCodes from './const/codes';
+
 class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFirstLanch: true,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isFirstLanch: true
-        }
-    }
-
-    componentDidMount() {
+  componentDidMount() {
         // 添加返回键监听
-        this._addBackAndroidListener(this.props.navigator);
-    }
+    this._addBackAndroidListener(this.props.navigator);
+  }
 
-    componentWillUnmount() {
+  componentWillUnmount() {
         // 移除返回键监听
-        this._removeBackAndroidListener();
-    }
+    this._removeBackAndroidListener();
+  }
 
-    componentWillMount() {
-        this._initApp();
-    }
+  componentWillMount() {
+    this._initApp();
+  }
 
-    render() {
-        if (this.state.isFirstLanch) {
-            return this._initFirstLanch();
-        } else {
-            return this._initNoFirstLanch();
-        }
+  render() {
+    if (this.state.isFirstLanch) {
+      return this._initFirstLanch();
     }
+    return this._initNoFirstLanch();
+  }
 
-    //跳转登录
-    _login() {
-        return (<Login navigator={this.props.navigator}/>)
-    }
 
-    //跳转splash
-    _splash() {
-        return (<Splash navigator={this.props.navigator}/>)
-    }
+    // 跳转登录
+  _login() {
+    return (<Login navigator={this.props.navigator} />);
+  }
 
-    //首次启动
-    _initFirstLanch() {
-        AppStorage.save(AppCodes.APP_LANCHER, AppCodes.APP_LANCHER_TYPE.NoFirst);
-        return this._splash();
-    }
+    // 跳转splash
+  _splash() {
+    return (<Splash navigator={this.props.navigator} />);
+  }
 
-    //非首次启动
-    _initNoFirstLanch() {
-        return this._login();
-    }
+    // 首次启动
+  _initFirstLanch() {
+    AppStorage.save(AppCodes.APP_LANCHER, AppCodes.APP_LANCHER_TYPE.NoFirst);
+    return this._splash();
+  }
 
-    //初始化app
-    _initApp() {
-        AppStorage.get(AppCodes.APP_LANCHER).then((value) => {
-            value = value ? value : AppCodes.APP_LANCHER_TYPE.First;
-            switch (value) {
-                case AppCodes.APP_LANCHER_TYPE.First:
-                    break;
-                case AppCodes.APP_LANCHER_TYPE.NoFirst:
-                    this.setState({
-                        isFirstLanch: false
-                    })
-                    break;
-                default:
-                    this.setState({
-                        isFirstLanch: false
-                    })
-            }
-        })
-    }
+    // 非首次启动
+  _initNoFirstLanch() {
+    return this._login();
+  }
+
+    // 初始化app
+  _initApp() {
+    AppStorage.get(AppCodes.APP_LANCHER).then((value) => {
+      value = value || AppCodes.APP_LANCHER_TYPE.First;
+      switch (value) {
+        case AppCodes.APP_LANCHER_TYPE.First:
+          break;
+        case AppCodes.APP_LANCHER_TYPE.NoFirst:
+          this.setState({
+              isFirstLanch: false,
+            });
+          break;
+        default:
+          this.setState({
+              isFirstLanch: false,
+            });
+      }
+    });
+  }
+
 
     // 监听返回键事件
-    _addBackAndroidListener(navigator) {
-        if (Platform.OS === 'android') {
-            BackHandler.addEventListener('hardwareBackPress', () => {
-                return this._onBackAndroid(navigator);
-            });
-        }
+  _addBackAndroidListener(navigator) {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', () => this._onBackAndroid(navigator));
     }
+  }
 
     // 移除监听
-    _removeBackAndroidListener() {
-        if (Platform.OS === 'android') {
-            BackHandler.removeEventListener('hardwareBackPress', () => {
-            });
-        }
+  _removeBackAndroidListener() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress', () => {
+      });
     }
+  }
 
 
     // 判断是返回上一页还是退出程序
-    _onBackAndroid(navigator) {
-        if (!navigator) return false;
-        const routers = navigator.getCurrentRoutes();
+  _onBackAndroid(navigator) {
+    if (!navigator) return false;
+    const routers = navigator.getCurrentRoutes();
         // 当前页面不为root页面时的处理
-        if (routers.length > 0) {
-            const top = routers[routers.length - 1];
-            ToastAndroid.show(JSON.stringify(top.component.displayName), ToastAndroid.SHORT);
-            switch (top.component.displayName) {
-                case 'Root':
-                    BackHandler.exitApp();
-                    return false;
-                case 'Login':
-                    BackHandler.exitApp();
-                    return false;
-                case 'Main':
-                    BackHandler.exitApp();
-                    return false;
-            }
-            if (top.ignoreBack || top.component.ignoreBack) {
+    if (routers.length > 0) {
+      const top = routers[routers.length - 1];
+      ToastAndroid.show(JSON.stringify(top.component.displayName), ToastAndroid.SHORT);
+      switch (top.component.displayName) {
+        case 'Root':
+          BackHandler.exitApp();
+          return false;
+        case 'Login':
+          BackHandler.exitApp();
+          return false;
+        case 'Main':
+          BackHandler.exitApp();
+          return false;
+      }
+      if (top.ignoreBack || top.component.ignoreBack) {
                 // 路由或组件上决定这个界面忽略back键
-                return true;
-            }
-            const handleBack = top.handleBack || top.component.handleBack;
-            if (handleBack) {
+        return true;
+      }
+      const handleBack = top.handleBack || top.component.handleBack;
+      if (handleBack) {
                 // 路由或组件上决定这个界面自行处理back键
-                return handleBack();
-            }
+        return handleBack();
+      }
             // 默认行为： 退出当前界面。
-            navigator.pop();
-            return true;
-        }
-
+      navigator.pop();
+      return true;
     }
+  }
 }
 
 module.exports = Root;
